138fca854d204b3c788149961ac05ad1a845c30e
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-12-13 15:16:08 +0300

    Add the initial setup for the project

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+const password = encodeURIComponent('Muya@.2006');
+export const mongoDBURL = `mongodb+srv://robertmbuguakanyoro:${password}@mern-nodejs.l1zwtgd.mongodb.net/books-collection?retryWrites=true&w=majority`;
